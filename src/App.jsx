42a26d2c206c96d627c90fb2f908db271fab65f8
Anderes <andres10000fky@gmail.com>
2024-06-20 12:26:01 -0400

    Todo

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import TarjetaCuatro from './componentes/TarjetaCuatro'
 function App() {
   return (
     <div className='cuerpo'>
+      <div className='letrita'>
+        <p className='nombre'>Exercicios</p>
+      </div>
       <TarjetaUno />
       <TarjetaDos />
       <TarjetaTres />
